9e2b5f92ca91624a687bc53ea587c88de4faafb2
Mario <marionikolov87@gmail.com>
2021-01-31 12:06:12 +0200

    add tasks component; add global state;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: 'Feed die Hunde',
+			day: 'Feb 5th at 2:30pm',
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: 'Feed die Katzen',
+			day: 'Feb 6th at 1:30pm',
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: 'Vet appointment',
+			day: 'Feb 5th at 2:30pm',
+			reminder: false,
+		},
+	]);
+
 	return (
 		<div className="container">
 			<Header />
+			<Tasks tasks={tasks} />
 		</div>
 	);
 }
