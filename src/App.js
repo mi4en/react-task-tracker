9174c4386deec4a01a8977ce60dd8f7ee76efcda
Mario <marionikolov87@gmail.com>
2021-02-01 08:47:29 +0200

    add addTask form; add new task to array oif tasks;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
 	const [tasks, setTasks] = useState([
@@ -24,6 +25,15 @@ function App() {
 		},
 	]);
 
+	// Add Task
+	const addTask = (task) => {
+		console.log('new task: ', task);
+		const id = Math.floor(Math.random() * 10000) + 1;
+
+		const newTask = { id, ...task };
+		setTasks([...tasks, newTask]);
+	};
+
 	// Delete Task
 	const deleteTask = (id) => {
 		console.log('delete: ', id);
@@ -39,6 +49,9 @@ function App() {
 	return (
 		<div className="container">
 			<Header />
+
+			<AddTask onAdd={addTask} />
+
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
